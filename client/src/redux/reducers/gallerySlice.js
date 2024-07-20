ab033eb005c322971093b6b520559b77c5d33cd3
Sangam <96041004+SANGAAM@users.noreply.github.com>
2024-07-21 00:15:53 +0530

    Update gallerySlice.js

--- client/src/redux/reducers/gallerySlice.js
+++ client/src/redux/reducers/gallerySlice.js
@@ -8,7 +8,7 @@ const initialValues = {
 export const getAllImages = createAsyncThunk(
   "images/fetchallimages",
   async () => {
-    const res = await axios.get("http://localhost:8000/api/v1/get/images");
+    const res = await axios.get("https://gallery-app-q5n5.onrender.com/api/v1/get/images");
     return res.data;
   }
 );
@@ -16,7 +16,7 @@ export const getAllImages = createAsyncThunk(
 export const getAllCategories = createAsyncThunk(
   "images/fetchallcategories",
   async () => {
-    const res = await axios.get("http://localhost:8000/api/v1/get/categories");
+    const res = await axios.get("https://gallery-app-q5n5.onrender.com/api/v1/get/categories");
     return res.data;
   }
 );
@@ -25,7 +25,7 @@ export const postNewCategory = createAsyncThunk(
   "images/postnewcategory",
   async (payload) => {
     const res = await axios.post(
-      "http://localhost:8000/api/v1/add/category",
+      "https://gallery-app-q5n5.onrender.com/api/v1/add/category",
       payload
     );
     return res.data;
@@ -36,7 +36,7 @@ export const postNewImage = createAsyncThunk(
   "images/postnewimage",
   async (payload) => {
     const res = await axios.post(
-      "http://localhost:8000/api/v1/upload/image",
+      "https://gallery-app-q5n5.onrender.com/api/v1/upload/image",
       payload
     );
     return res.data;
@@ -47,7 +47,7 @@ export const getSingleImage = createAsyncThunk(
   "images/getsingleImageData",
   async (payload) => {
     const res = await axios.get(
-      `http://localhost:8000/api/v1/get/singleimage?category=${payload}`
+      `https://gallery-app-q5n5.onrender.com/api/v1/get/singleimage?category=${payload}`
     );
     return res.data;
   }
