2e0ca446f768751e091afc16fdb31731989dbb11
Sangam <96041004+SANGAAM@users.noreply.github.com>
2024-07-21 00:40:06 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongo = async () => {
   const res = await mongoose.connect(
+    mongoose.set('strictQuery', false);
     "mongodb://0.0.0.0/mernchat"
   );
   if (res) {
