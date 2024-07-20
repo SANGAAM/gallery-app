30505645b92de48f838ffdbb2e7edad200aebcbe
Sangam <96041004+SANGAAM@users.noreply.github.com>
2024-07-21 00:16:45 +0530

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -52,7 +52,7 @@ const Home = () => {
             return (
               <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe my-4">
                 <img
-                  src={`http://localhost:8000/${item.name}`}
+                  src={`https://gallery-app-q5n5.onrender.com/${item.name}`}
                   class="img img-responsive"
                   height="300px"
                   width="300px"
